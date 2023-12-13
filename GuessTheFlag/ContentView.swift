4d9679f1cc6cb33849739d66d256d6df103e867d
filecc <fil.gia@live.it>
2023-12-13 21:13:21 +0100

    trying things

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+            VStack(spacing: 0) {
+                /*RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 800)*/
+                AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+                }
+            
+            VStack {
+                
+                HStack{
+                    HStack (spacing: 20){
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                    }
+                }
+                HStack{
+                    HStack (spacing: 20){
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                    }
+                }
+                HStack{
+                    HStack (spacing: 20){
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        Rectangle()
+                            .frame(width: 50, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                    }
+                }
+                
+            }
+            .padding(20)
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+        }.ignoresSafeArea()
+        
+        
     }
 }
 
